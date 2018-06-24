c1564c4f92fc1329ed502a6bff62229c9cc6e31d
Kovay <aidynn2010@gmail.com>
2018-06-23 18:08:28 -0700

    Add files via upload

--- Roll_A_Die.py
+++ Roll_A_Die.py
@@ -18,31 +18,41 @@ while running == "yes":
 
     # code for rolling a die
     if command == "Roll":
+        # asks user how many sides the dice they are rolling should have
         print("How many sides should the die have?")
         sides = input()
+        # checks if user input a number. if not, prints error message
         if sides.isnumeric() == False:
             print("Im not sure what you mean.")
             command = input()
             continue
+        # asks user how many dice they want to roll
         print("How many dice do you want to roll?")
         die_num = input()
+        # checks if user input a number. if not, prints error message
         if die_num.isnumeric() == False:
             print("Im not sure what you mean.")
             command = input()
             continue
+        # prints a random number from 1, to the number of sides the user
+        # selected, and loops for the number of dice the user decided to roll
         for num_of_dice in range(1, int(die_num) + 1):
             print(random.randint(1, int(sides)))
         command = input()
         continue
 
     # code for flipping a coin
+    # asks user how many coins they want to flip
     if command == "Flip a coin":
         print("How many coins do you want to flip?")
         coin_num = input()
+        # checks if user input a number. if not, prints error message
         if coin_num.isnumeric() == False:
             print("im not sure what you mean.")
             command = input()
             continue
+        # prints wether the coin landed on heads or tails, for the number of
+        # coins the user wanted to flip
         for coin in range(1, int(coin_num) + 1):
             face = random.randint(1, 2)
             if face == 1:
@@ -52,6 +62,7 @@ while running == "yes":
         command = input()
         continue
 
+    # error message for when user inputs unknown command
     if command != "Flip a coin" or "Roll" or "Quit":
         print("Im not sure what you mean")
         command = input()
